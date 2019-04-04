33455499226bc11bb6737416331791435d65b0ab
jiaktgsm <jiaktgsm@jcom.zaq.ne.jp>
2019-04-04 18:27:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewapp/MyAdapter.java
@@ -0,0 +1,63 @@
+package com.example.recyclerviewapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<String> values;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtHeader;
+        public TextView txtFooter;
+        public View layout;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout = v;
+            txtHeader = (TextView) v.findViewById(R.id.firstLine);
+            txtFooter = (TextView) v.findViewById(R.id.secondLine);
+        }
+    }
+
+    public void add(int position, String item) {
+        values.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position) {
+        values.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public MyAdapter(List<String> myDataset) {
+        values = myDataset;
+    }
+
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.row_layout, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    public void onBindViewHolder(ViewHolder holder, final int position){
+        final String name = values.get(position);
+        holder.txtHeader.setText(name);
+        holder.txtHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(position);
+            }
+        });
+        holder.txtFooter.setText("Footer: " + name);
+    }
+
+    public int getItemCount(){
+        return values.size();
+    }
+}
